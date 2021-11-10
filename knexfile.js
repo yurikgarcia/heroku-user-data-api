0e19243c53e8660df9b9fba21455c893d867c038
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-11-10 10:27:38 -0700

    add SSL to prod connection

--- knexfile.js
+++ knexfile.js
@@ -2,31 +2,29 @@
 require("dotenv").config();
 
 module.exports = {
-
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL+"?ssl=true",
+    client: "pg",
+    connection: process.env.DATABASE_URL + "?ssl=true",
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: "pg",
+    connection: process.env.DATABASE_URL + "?ssl=true",
   },
-
 };
