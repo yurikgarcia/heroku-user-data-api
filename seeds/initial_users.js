90d70e75b3efe79a8d832aff7978b6619af0fad6
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-11-10 10:48:41 -0700

    create better id assignment for seed

--- seeds/initial_users.js
+++ seeds/initial_users.js
@@ -1,9 +1,10 @@
 const dummy_data = require('../dummy_data')
+dummy_data_with_ids = dummy_data.map((dataPoint,ind)=>({id:ind+1,...dataPoint}))
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert(dummy_data);
+      return knex('users').insert(dummy_data_with_ids);
     });
 };
